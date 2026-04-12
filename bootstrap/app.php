2e017e95a321dafc498a3890707426e920211af4
kinanti-art <karpfunction@gmail.com>
2026-04-13 01:56:51 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+    
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(at: '*'); // Izinkan semua proxy dari Railway
+})
